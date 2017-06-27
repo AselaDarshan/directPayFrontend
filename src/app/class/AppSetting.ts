239fe1fedc78a27931ca3c1da479642ec6e5897a
AselaDarshan <aseladarshan@gmail.com>
2017-06-27 23:35:30 +0530

    Show more deails in slip

--- src/app/class/AppSetting.ts
+++ src/app/class/AppSetting.ts
@@ -2,7 +2,7 @@
 export class AppSettings {
 
   // public static DIRECT_PAY_ENDPOINT='http://13.58.144.197';
-  public static DIRECT_PAY_ENDPOINT='http://localhost:8000';
+  public static DIRECT_PAY_ENDPOINT='http://192.168.8.102:8000';
   public static API_M = 'https://localhost:8243';
   public static IS = 'https://localhost:9446';
   public static accessToken;
